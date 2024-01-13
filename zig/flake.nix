5337b4d4975a46fd97185223659bc0181c899e0f
tacogips <me@tacogips.me>
2024-01-13 22:50:12 +0900

    add zig

--- zig/flake.nix
+++ zig/flake.nix
@@ -1,12 +1,39 @@
 {
-  output = { ... }: {
+  description = "my zig project";
 
-    templates = {
-      rust-fenix = {
-        path = ./rust-fenix;
-        description = "rust overlay with fenix";
-      };
-
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
   };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , zig-overlay
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      zig-version = "master";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      zig = zig-overlay.packages.${system}.${zig-version};
+    in
+
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs =
+          (with pkgs;
+          [
+            just
+            zig
+          ]);
+
+        shellHook = ''
+        '';
+      };
+    });
 }
