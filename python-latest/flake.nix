6059e6721f9761d591558d4e206e78c6e6256cd3
tacogips <me@tacogips.me>
2024-06-11 17:29:48 +0900

    remove comment

--- python-latest/flake.nix
+++ python-latest/flake.nix
@@ -32,7 +32,6 @@
                 (import ./pydep.nix {
                   inherit pypkg;
                 })
-                # [ pypkg.pip pypkg.flake8 pypkg.pep8-naming pypkg.black pypkg.GitPython pypkg.pytz pypkg.beautifulsoup4 pypkg.polars ]
 
               ))
 
